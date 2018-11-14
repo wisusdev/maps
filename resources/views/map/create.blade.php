7863f98c6975118680ec9f54210facce32c8a72a
Jesus Avelar <jesus.avelar.nav@gmail.com>
2018-11-14 13:26:19 -0600

    Move .env

--- resources/views/map/create.blade.php
+++ resources/views/map/create.blade.php
@@ -63,7 +63,7 @@
 @endsection
 
 @section('header')
-    <script type="text/javascript" src='https://maps.googleapis.com/maps/api/js?key=AIzaSyCICVFZg9PawAeVO5oH_BRdE7IEu93eG8E&libraries=places'></script>
+    <script type="text/javascript" src='https://maps.googleapis.com/maps/api/js?&libraries=places'></script>
 
     <script type="text/javascript" src="{{asset('js/locationpicker.jquery.js')}}"></script>
 @endsection
